c35197f72fe5b15f470eee955378f473bc8fc6b0
Marks686 <17829791810@163.com>
2023-06-22 20:32:30 +0800

    多线程

--- J-20230612-Thread/src/thread/ThreadDemo3.java
+++ J-20230612-Thread/src/thread/ThreadDemo3.java
@@ -6,7 +6,7 @@ package thread;
  * Date: 2023-06-11
  * Time: 16:21
  */
-//使用匿名内部类
+//使用匿名内部类.
 public class ThreadDemo3 {
     public static void main(String[] args) {
         Thread t = new Thread()
