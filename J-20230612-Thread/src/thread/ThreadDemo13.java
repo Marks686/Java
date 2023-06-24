6826b684d7f40397df4ddd4617070b5e3548c4b6
Marks686 <17829791810@163.com>
2023-06-24 17:42:35 +0800

    多线程

---
+++ J-20230612-Thread/src/thread/ThreadDemo13.java
@@ -0,0 +1,42 @@
+package thread;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * Date: 2023-06-24
+ * Time: 14:39
+ */
+class Counter{
+    public static int count = 0;
+     public synchronized static void add(){
+        count++;
+    }
+}
+public class ThreadDemo13 {
+    public static void main(String[] args) {
+        Counter counter = new Counter();
+        //搞两个线程 分别针对counter 来调用5w次add方法
+        Thread t1 = new Thread(()->{
+            for (int i = 0; i < 50000; i++) {
+                counter.add();
+            }
+        });
+        Thread t2 = new Thread(()->{
+            for (int i = 0; i < 50000; i++) {
+                counter.add();
+            }
+        });
+        t1.start();
+        t2.start();
+        //等待两个线程结束
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //打印最终的count值
+        System.out.println("count = " + counter.count);
+    }
+}
