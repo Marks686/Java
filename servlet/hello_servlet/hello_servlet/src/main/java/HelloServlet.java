16de9965e46780fb85146353dec9563b7d0212b5
Marks686 <17829791810@163.com>
2023-08-14 09:09:57 +0800

    Servlet

---
+++ servlet/hello_servlet/hello_servlet/src/main/java/HelloServlet.java
@@ -0,0 +1,24 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * Date: 2023-08-13
+ * Time: 10:02
+ */
+//下面的注解把当前类和一个HTTP请求的路径关联起来
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //这个只是在服务器的控制台打印
+        System.out.println("hello world");
+        //要想把hello world返回到客户端,需要使用下面的代码
+        //getWriter 会得到一个Writer对象
+        resp.getWriter().write("hello world");
+    }
+}
