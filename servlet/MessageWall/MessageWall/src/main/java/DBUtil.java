ee7d085d678ffaed4d3f2e085bdbac9e60a65a2d
Marks686 <17829791810@163.com>
2023-08-16 23:05:00 +0800

    表白墙

---
+++ servlet/MessageWall/MessageWall/src/main/java/DBUtil.java
@@ -0,0 +1,35 @@
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * Date: 2023-08-16
+ * Time: 11:01
+ */
+//通过这个类,把数据库连接过程封装一下,此处把这个类作为一个工具类,提供static方法 供其他代码来调用.
+public class DBUtil {
+    //静态成员跟随类对象 类对象在整个进程中只有唯一一份
+    //静态成员相当于也是唯一的实例 (单例模式 饿汉模式)
+    private static DataSource dataSource = new MysqlDataSource();
+
+    static {
+        //使用静态代码块 针对dataSourse初始化
+        ((MysqlDataSource)dataSource).setURL("jdbc:mysql://127.0.0.1:3306/java106?characterEncoding=utf8&useSSL=false");
+        ((MysqlDataSource)dataSource).setUser("root");
+        ((MysqlDataSource)dataSource).setPassword("123456");
+    }
+
+    //通过这个方法建立连接
+    public static Connection getConnection(){
+
+    }
+
+    //通过这个方法断开连接,释放资源
+    public static void close(Connection connection, PreparedStatement statement, ResultSet resultSet){
+
+    }
+}
