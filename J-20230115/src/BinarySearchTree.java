458e5002f23c9024ed751ce6fb08ab4aaab2b9b0
Marks686 <17829791810@163.com>
2023-09-06 23:30:43 +0800

    Map and Set

---
+++ J-20230115/src/BinarySearchTree.java
@@ -0,0 +1,102 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * Date: 2023-09-06
+ * Time: 19:16
+ */
+public class BinarySearchTree {
+    static class TreeNode{
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val){
+            this.val = val;
+        }
+    }
+
+    public TreeNode root = null;
+
+    //查找二叉搜索数中指定的val值
+    public TreeNode find(int val){
+        TreeNode cur = root;
+        while (cur != null){
+            if (cur.val == val){
+                return cur;
+            }else if (cur.val > val){
+                cur = cur.left;
+            }else {
+                cur = cur.right;
+            }
+        }
+        return null;
+    }
+    //插入一个数据
+    public void insert(int val){
+        if (root == null){
+            root = new TreeNode(val);
+            return;
+        }
+        TreeNode cur = root;
+        TreeNode parent = null;
+        while (cur != null){
+            if (cur.val < val){
+                parent = cur;
+                cur = cur.right;
+            }else if (cur.val > val){
+                parent = cur;
+                cur = cur.left;
+            } else {
+                return;
+            }
+        }
+        TreeNode node = new TreeNode(val);
+        if (parent.val < val){
+            parent.right = node;
+        }else {
+            parent.left = node;
+        }
+    }
+
+    public void inorder(TreeNode root){
+        if (root == null){
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.val+" ");
+        inorder(root.right);
+    }
+    //删除值为val的节点
+    public void remove(int val){
+        TreeNode cur = root;
+        TreeNode parent = null;
+        while (cur != null){
+            if (cur.val == val){
+                removeNode(parent,cur);
+                return;
+            }else if (cur.val < val){
+                parent = cur;
+                cur = cur.right;
+            }else {
+                parent = cur;
+                cur = cur.left;
+            }
+        }
+    }
+
+    private void removeNode(TreeNode parent, TreeNode cur) {
+        if (cur.left == null){
+            if (cur == root){
+                root = cur.right;
+            }else if (parent.left == cur){
+                parent.left = cur.right;
+            }else {
+                parent.right = cur.right;
+            }
+        } else if (cur.right == null){
+
+        }else {
+
+        }
+    }
+
+}
---
+++ J-20230115/src/Test.java
@@ -0,0 +1,21 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * Date: 2023-09-06
+ * Time: 19:15
+ */
+public class Test {
+    public static void main(String[] args) {
+        BinarySearchTree binarySearchTree = new BinarySearchTree();
+        binarySearchTree.insert(12);
+        binarySearchTree.insert(21);
+        binarySearchTree.insert(5);
+        binarySearchTree.insert(18);
+        binarySearchTree.insert(9);
+
+        binarySearchTree.inorder(binarySearchTree.root);
+        System.out.println();
+        BinarySearchTree.TreeNode ret = binarySearchTree.find(12);
+        System.out.println(ret.val+" ");
+    }
+}
