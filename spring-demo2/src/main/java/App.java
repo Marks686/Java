93478ded175e14bf71a3b08def66e281cb394fed
Marks686 <17829791810@163.com>
2023-09-14 00:05:09 +0800

    spring创建和使用

---
+++ spring-demo2/src/main/java/App.java
@@ -0,0 +1,19 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * Date: 2023-09-13
+ * Time: 22:29
+ */
+public class App {
+    public static void main(String[] args) {
+        //1.得到Spring的上下文 创建的时候需要配置Spring 配置信息
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("spring-config.xml");
+        //2.从Spring中取出Bean对象
+        User user = (User) context.getBean("user");
+        //3.使用Bean(可选)
+        System.out.println(user.sayHi());
+    }
+}
---
+++ spring-demo2/src/main/java/User.java
@@ -0,0 +1,11 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * Date: 2023-09-13
+ * Time: 22:36
+ */
+public class User {
+    public String sayHi(){
+        return "hello, world.";
+    }
+}
