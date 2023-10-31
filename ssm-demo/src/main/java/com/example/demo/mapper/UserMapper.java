f03ce83424e159a5408cbfe6c42269d8fc56c0f3
Marks686 <17829791810@163.com>
2023-10-31 17:50:04 +0800

    ssm-demo

--- ssm-demo/src/main/java/com/example/demo/mapper/UserMapper.java
+++ ssm-demo/src/main/java/com/example/demo/mapper/UserMapper.java
@@ -8,13 +8,39 @@ import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
- * Description: 根据用户 id 查询用户信息
+ * Description:
  * Date: 2023-10-25
  * Time: 23:12
  */
 @Mapper
 public interface UserMapper {
+    /* 根据id查询用户信息 */
     Userinfo getUserById(@Param("userId") Integer userId);
 
+    List<Userinfo> getUserByName(@Param("username") String username);
+
+    /* 查询全部 */
     List<Userinfo> getAll();
+
+    /* 添加用户对象 */
+    int add(Userinfo userinfo);
+
+    /* 添加用户 并返回用户 id*/
+    int addGetId(Userinfo userinfo);
+
+    /* 修改用户 */
+    int upUserName(Userinfo userinfo);
+
+    /* 根据用户id 删除 用户信息*/
+    int delById(@Param("id") Integer id);
+
+    /* 在查询的时候根据 id 大小排序进行显示 */
+    List<Userinfo> getListByOrder(@Param("order") String order);
+
+    /**/
+    Userinfo login(@Param("username") String username,
+                   @Param("password") String password);
+
+    List<Userinfo> getListByName(@Param("username") String username);
 }
+
--- ssm-demo/src/test/java/com/example/demo/mapper/UserMapperTest.java
+++ ssm-demo/src/test/java/com/example/demo/mapper/UserMapperTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,7 +18,8 @@ import static org.junit.jupiter.api.Assertions.*;
  * Date: 2023-10-29
  * Time: 19:18
  */
-@SpringBootTest
+@SpringBootTest // 表明当前单元测试是运行在 Spring Boot 环境中的
+//@Transactional
 class UserMapperTest {
 
     @Autowired
@@ -34,5 +37,85 @@ class UserMapperTest {
     void getAll() {
         List<Userinfo> list = userMapper.getAll();
         Assertions.assertEquals(1,list.size());
+    }
+
+    @Test
+    void add() {
+        Userinfo userinfo = new Userinfo();
+        userinfo.setUsername("重八");
+        userinfo.setPassword("1234567");
+        userinfo.setCreatetime(LocalDateTime.now());
+        userinfo.setUpdatetime(LocalDateTime.now());
+        // 调用 mybatis 添加方法执行添加操作
+        int result = userMapper.add(userinfo);
+        System.out.println("添加: " + result);
+
+        int uid = userinfo.getId();
+        System.out.println("用户ID: " + uid);
+        Assertions.assertEquals(1,result);
+    }
+
+    @Test
+    void addGetId() {
+        Userinfo userinfo = new Userinfo();
+        userinfo.setUsername("井七");
+        userinfo.setPassword("1234567");
+        userinfo.setCreatetime(LocalDateTime.now());
+        userinfo.setUpdatetime(LocalDateTime.now());
+        // 调用 mybatis 添加方法执行添加操作
+        int result = userMapper.addGetId(userinfo);
+        System.out.println("添加: " + result);
+
+        int uid = userinfo.getId();
+        System.out.println("用户ID: " + uid);
+        Assertions.assertEquals(1,result);
+    }
+
+    @Test
+    void upUserName() {
+        // 构建测试数据
+        Userinfo userinfo = new Userinfo();
+        userinfo.setId(5);
+        userinfo.setUsername("老老六");
+        int result = userMapper.upUserName(userinfo);
+        System.out.println("修改: " + result);
+        Assertions.assertEquals(1,result);
+    }
+
+    @Test
+    void delById() {
+        Integer id = 4;
+        int result = userMapper.delById(id);
+        System.out.println("删除: " + result);
+        Assertions.assertEquals(1,result);
+
+    }
+
+    @Test
+    void getUserByName() {
+        List<Userinfo> list = userMapper.getUserByName("张三");
+        System.out.println("查询: " + list.size());
+
+    }
+
+    @Test
+    void getListByOrder() {
+        List<Userinfo> list = userMapper.getListByOrder("desc");
+        System.out.println(list);
+    }
+
+    @Test
+    void login() {
+        String username = "admin";
+        String password = "' or 1 ='1"; // sql 注入[安全问题]
+        Userinfo userinfo = userMapper.login(username,password);
+        System.out.println("登录状态: " + (userinfo==null ? "失败" : "成功"));
+    }
+
+    @Test
+    void getListByName() {
+        String username = "m";
+        List<Userinfo> list = userMapper.getListByName(username);
+        System.out.println("list: " + list);
     }
 }
