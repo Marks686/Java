2f1826cfde6d7c4445eff8e800d8797a30a5a3c6
Marks686 <17829791810@163.com>
2024-03-07 10:57:00 +0800

    map set

--- J-20240306-map-set/src/Test.java
+++ J-20240306-map-set/src/Test.java
@@ -1,9 +1,6 @@
 import binarysearchtree.BinarySearchTree;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
+import java.util.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -50,9 +47,17 @@ public class Test {
     }
 
     public static void main(String[] args) {
+        Set<String> set = new TreeSet<>();
+        set.add("sunny");
+        set.add("hello");
+        set.add("the");
+        System.out.println(set);
 
+        Iterator<String> iterator = set.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
     }
 
 
-//        Map<String,Integer> map2 = new HashMap<>();
 }
