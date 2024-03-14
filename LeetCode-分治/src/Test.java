f56603c7b930d2881f07dd54b26c8992a820cbe5
Marks686 <17829791810@163.com>
2024-03-14 23:29:11 +0800

    分治算法

--- LeetCode-分治/src/Test.java
+++ LeetCode-分治/src/Test.java
@@ -57,4 +57,22 @@ public class Test {
             nums[j] = t;
         }
     }
+    class Solution
+    {
+        long prev = Long.MIN_VALUE;
+        public boolean isValidBST(TreeNode root)
+        {
+            if(root == null) return true;
+            boolean left = isValidBST(root.left);
+            // 剪枝
+            if(left == false) return false;
+            boolean cur = false;
+            if(root.val > prev) cur = true;
+            if(cur == false) return false;
+            prev = root.val;
+            boolean right = isValidBST(root.right);
+            return left && cur && right;
+        }
+    }
+
 }
