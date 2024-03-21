bff85bfc3ea33a4264f9e38671fde629ef6eb0dc
Marks686 <17829791810@163.com>
2024-03-21 23:37:04 +0800

    链表

--- LeetCode-链表/src/Test.java
+++ LeetCode-链表/src/Test.java
@@ -68,4 +68,36 @@ public class Test {
             return newHead.next;
         }
     }
+    lass Solution
+    {
+        public int minNumberOfFrogs(String c)
+        {
+            char[] croakOfFrogs = c.toCharArray();
+            String t = "croak";
+            int n = t.length();
+            int[] hash = new int[n]; // 数组模拟哈希表
+            Map<Character, Integer> index = new HashMap<>(); // [x, x这个字符对应的下标
+            for(int i = 0; i < n; i++)
+                index.put(t.charAt(i), i);
+            for(char ch : croakOfFrogs)
+            {
+                if(ch == t.charAt(0))
+                {
+                    if(hash[n - 1] != 0) hash[n - 1]--;
+                    hash[0]++;
+                }
+                else
+                {
+                    int i = index.get(ch);
+                    if(hash[i - 1] == 0) return -1;
+                    hash[i - 1]--; hash[i]++;
+                }
+            }
+            for(int i = 0; i < n - 1; i++)
+                if(hash[i] != 0)
+                    return -1;
+
+            return hash[n - 1];
+        }
+    }
 }
