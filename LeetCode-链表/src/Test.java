9465d59d1fcbe063a5c35d63b583fdd829ee4444
Marks686 <17829791810@163.com>
2024-03-31 16:05:33 +0800

    链表21.2.24.143.23.25

--- LeetCode-链表/src/Test.java
+++ LeetCode-链表/src/Test.java
@@ -174,5 +174,85 @@ public class Test {
             return dummyHead.next;
         }
     }
+//    23. 合并 K 个升序链表
+    class Solution6 {
+        public ListNode mergeKLists(ListNode[] lists) {
+            return merge(lists,0,lists.length - 1);
+        }
+
+        public ListNode merge(ListNode[] lists,int left, int right){
+            if(left > right) return null;
+            if(left == right) return lists[left];
+
+            //1.先平分数组
+            int mid = (left + right)/2;
+            //[left ,mid] [mid + 1,right]
+
+            // 2.递归处理左右两部分
+            ListNode l1 = merge(lists,left,mid);
+            ListNode l2 = merge(lists,mid+1,right);
+
+            //3.合并两个有序链表
+            return mergeTwoList(l1,l2);
+        }
+        public ListNode mergeTwoList(ListNode l1,ListNode l2){
+            if(l1 == null) return l2;
+            if(l2 == null) return l1;
+
+            // 合并两个有序链表
+            ListNode head = new ListNode(0);
+            ListNode cur1 = l1,cur2 = l2,prev = head;
+            while(cur1 != null && cur2 != null){
+                if(cur1.val <= cur2.val){
+                    prev.next = cur1;
+                    prev = cur1;
+                    cur1 = cur1.next;
+                }else{
+                    prev.next = cur2;
+                    prev = cur2;
+                    cur2 = cur2.next;
+                }
+            }
+            if(cur1 != null){
+                prev.next = cur1;
+            }
+            if(cur2 != null){
+                prev.next = cur2;
+            }
+
+            return head.next;
+        }
+    }
+
+    class Solution7 {
+        public ListNode reverseKGroup(ListNode head, int k) {
+            // 1.先求出需要逆序多少组
+            int n = 0;
+            ListNode cur = head;
+            while(cur != null){
+                cur = cur.next;
+                n++;
+            }
+            n /= k;
+            // 2.重复n次 ：长度为k的链表的逆序
+            ListNode newHead = new ListNode(0);
+            ListNode prev = newHead;
+            cur = head;
+            for(int i = 0; i < n; i++){
+                ListNode tmp = cur;
+                for(int j = 0; j < k; j++){
+                    // 头插
+                    ListNode next = cur.next;
+                    cur.next = prev.next;
+                    prev.next = cur;
+                    cur = next;
+                }
+                prev = tmp;
+            }
+            // 把后面不需要逆序的连接上
+            prev.next = cur;
+            return newHead.next;
+        }
+    }
 
 }
