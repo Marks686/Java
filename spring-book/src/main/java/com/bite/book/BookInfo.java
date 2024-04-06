2b45402f21bbb5672279fdccdeb1f9d40cc9ddc7
Marks686 <17829791810@163.com>
2024-04-06 19:37:15 +0800

    spring-book

---
+++ spring-book/src/main/java/com/bite/book/BookInfo.java
@@ -0,0 +1,35 @@
+package com.bite.book;
+
+
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class BookInfo {
+    private Integer id;
+    //书名
+    private String bookName;
+
+    //作者
+    private String author;
+
+    //数量
+    private Integer count;
+
+    //价格用BigDecimal
+    private BigDecimal price;
+
+    // 出版社
+    private String publish;
+
+    //状态 不存汉字
+    private Integer status;//1-可借阅 2-不可借阅
+
+    private String statusCN;
+
+
+
+
+}
