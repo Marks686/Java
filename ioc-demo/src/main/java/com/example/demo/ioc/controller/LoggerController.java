fd68c2e2fe1311de086dcce5929075f4b6163bdb
Marks686 <17829791810@163.com>
2024-04-19 15:27:11 +0800

    ioc-demo,增加了日志的使用代码(使用注解的方式)

--- ioc-demo/src/main/java/com/example/demo/ioc/controller/LoggerController.java
+++ ioc-demo/src/main/java/com/example/demo/ioc/controller/LoggerController.java
@@ -1,20 +1,31 @@
 package com.example.demo.ioc.controller;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.PostConstruct;
-
+@Slf4j
 @RestController
 public class LoggerController {
-    private static Logger logger = LoggerFactory.getLogger(LoggerController.class);
+//    private static Logger logger = LoggerFactory.getLogger(LoggerController.class);
+
+//    @PostConstruct
+//    public void print(){
+//        System.out.println("打印日志");
+//        logger.info("我是日志框架打印的日志");
+//        logger.error("error");
+//        logger.warn("warn");
+//        logger.debug("debug");
+//        logger.trace("trace");
+//    }
 
     @PostConstruct
     public void print(){
         System.out.println("打印日志");
-        logger.info("我是日志框架打印的日志");
-        logger.error("error");
-        logger.warn("warn");
-        logger.debug("debug");
-        logger.trace("trace");
+        log.info("我是日志框架打印的日志");
+        log.error("error");
+        log.warn("warn");
+        log.debug("debug");
+        log.trace("trace");
     }
 }
