84176c7e7147de5866e454947150c8c0837c3829
Marks686 <17829791810@163.com>
2024-09-26 21:23:12 +0800

    Linked list + double pointer + hash table + sliding window + binary search + prefix sum

--- practice/src/Test.java
+++ practice/src/Test.java
@@ -741,3 +741,86 @@ class Solution31 {
         return left == records[left] ? left+1 : left;
     }
 }
+
+class Solution32 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int q = in.nextInt();
+        int[] arr = new int[n+1];
+        for(int i = 1; i <= n; i++){
+            arr[i] = in.nextInt();
+        }
+
+        long[] dp = new long[n+1];
+        for(int i = 1; i <= n; i++){
+            dp[i] = dp[i-1] + arr[i];
+        }
+        while(q > 0){
+            int l = in.nextInt();
+            int r = in.nextInt();
+            System.out.println(dp[r]-dp[l-1]);
+            q--;
+        }
+    }
+}
+
+
+// 注意类名必须为 Main, 不要有任何 package xxx 信息
+class Solution33 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int q = in.nextInt();
+        int[][] arr = new int[n+1][m+1];
+
+        for(int i = 1; i <= n; i++)
+            for(int j = 1; j <= m; j++)
+                arr[i][j] = in.nextInt();
+
+        long[][] dp = new long[n+1][m+1];
+        for(int i = 1; i <= n; i++)
+            for(int j = 1; j <= m; j++)
+                dp[i][j] = dp[i-1][j] + dp[i][j-1] + arr[i][j] - dp[i-1][j-1];
+
+
+
+        while(q > 0){
+            int x1 = in.nextInt();
+            int y1 = in.nextInt();
+            int x2 = in.nextInt();
+            int y2 = in.nextInt();
+
+            System.out.println(dp[x2][y2] - dp[x1-1][y2] - dp[x2][y1-1] + dp[x1-1][y1-1]);
+            q--;
+        }
+    }
+}
+
+class Solution34 {
+    public int pivotIndex(int[] nums) {
+        int n = nums.length;
+        int[] f = new int[n];
+        int[] g = new int[n];
+
+        // 1.预处理一下前缀和数组以及后缀和数组
+        for(int i = 1; i < n; i++)
+        {
+            f[i] = f[i-1] + nums[i-1];
+        }
+        for(int i = n - 2; i >= 0; i--)
+        {
+            g[i] = g[i+1] + nums[i+1];
+        }
+        // 2.使用
+        for(int i = 0; i < n; i++)
+        {
+            if(f[i] == g[i])
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
