434de91b885ab52de31dd33b6a6a4b1512231920
Marks686 <17829791810@163.com>
2024-09-27 19:25:19 +0800

    Linked list + double pointer + hash table + sliding window + binary search + prefix sum

--- practice/src/Test.java
+++ practice/src/Test.java
@@ -824,3 +824,26 @@ class Solution34 {
         return -1;
     }
 }
+
+class Solution35 {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] f = new int[n];
+        int[] g = new int[n];
+        f[0] = g[n-1] = 1;
+
+        for(int i = 1; i < n; i++){
+            f[i] =  f[i-1] * nums[i-1];
+        }
+        for(int i = n-2; i >= 0; i--){
+            g[i] =  g[i+1] * nums[i+1];
+        }
+
+        int[] ret = new int[n];
+        for(int i = 0; i < n; i++){
+            ret[i] = f[i] * g[i];
+        }
+
+        return ret;
+    }
+}
